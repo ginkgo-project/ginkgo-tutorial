78a3ad169770aecd1ce25c57708bec2af0deb4b1
Marcel Koch <marcel.koch@kit.edu>
2023-04-03 16:11:03 +0200

    read matrix example

---
+++ hello-world/read-matrix.cpp
@@ -0,0 +1,13 @@
+#include <ginkgo/ginkgo.hpp>
+#include <fstream>
+
+int main() {
+    using Mtx = gko::matrix::Csr<double, int>;
+    using Vec = gko::matrix::Dense<double>;
+    auto exec = gko::CudaExecutor::create(0, gko::OmpExecutor::create());
+    auto mtx = gko::read<Mtx>(std::ifstream{"data/A.mtx"}, exec);
+    auto x = gko::read<Vec>(std::ifstream{"data/x.mtx"}, exec);
+    auto y = gko::read<Vec>(std::ifstream{"data/y.mtx"}, exec);
+    mtx->apply(x, y);
+    gko::write(std::cout, y);
+}
