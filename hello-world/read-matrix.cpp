d81bf1d0522690b0ca0dff220e2b7694a973a7e7
Marcel Koch <marcel.koch@kit.edu>
2023-04-11 10:04:25 +0200

    always use reference executor initially

--- hello-world/read-matrix.cpp
+++ hello-world/read-matrix.cpp
@@ -4,7 +4,7 @@
 int main() {
     using Mtx = gko::matrix::Csr<double, int>;
     using Vec = gko::matrix::Dense<double>;
-    auto exec = gko::CudaExecutor::create(0, gko::OmpExecutor::create());
+    auto exec = gko::ReferenceExecutor::create();
     auto mtx = gko::read<Mtx>(std::ifstream{"data/A.mtx"}, exec);
     auto x = gko::read<Vec>(std::ifstream{"data/x.mtx"}, exec);
     auto y = gko::read<Vec>(std::ifstream{"data/y.mtx"}, exec);
